682e60b554a702010269fd99f3708825fde6b8be
Your Name <shivamarora.07.sa@gmail.com>
2024-01-20 22:58:54 +0530

    fetching

--- fetcho.js
+++ fetcho.js
@@ -1,4 +1,12 @@
 fetch(`https://api.github.com/users/shivamarora01`)
-.then((res) => res.json)
-let data = res;
-console.log(data);
+//response mai baaki extra bhi aata hai
+.then((res) => res.json())
+//islieye its converted into json file phle
+//fetch return krta h promise
+//.then .catch se throw krskte output
+.then((res) => {
+    console.log(res);
+})
+.catch((error) => {
+    console.log("Error Occured : " ,  error)
+})

49f2da21ad753b103ac16f29b8313eeb5e24f39f
Shivam <shivamarora.07.sa@gmail.com>
2026-02-10 08:08:52 +0530

    Uncontrolled Input using useRef

--- 03tailwindprops/src/ExtraButton.jsx
+++ 03tailwindprops/src/ExtraButton.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 //by using const, now its a arrow function
 export const ExtraButton = (props) => {
+    const [count, setCount] = useState(1);
+    let har = 3;
+    //never change state directly when rendering, do it in a event trigger or effect
+    // setCount(count => count+1);
     return (
-        <p>Hi, I'm that extra button, {props.children}</p>
+        <>
+        <p>Hi, I'm that extra button {props.children}</p>
+        <p>{count}</p>
+        {/* //use arrow function so that the setCount only runs on trigger, not on render, render creates infinite loop because it think states are changing */}
+        <button onClick={() => setCount(count+1)}>Increase count</button>
+        </>
     )
 }
